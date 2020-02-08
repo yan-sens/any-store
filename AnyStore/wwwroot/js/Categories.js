2be01f8357ae7af755a87aa78fed1f1d5a74131d
Andrey <andrey.yazik@sagittarius-marketing.com>
2020-02-09 01:00:45 +0200

    - Added kendo images - Added styles - A little refactor

--- AnyStore/wwwroot/js/Categories.js
+++ AnyStore/wwwroot/js/Categories.js
@@ -3,33 +3,47 @@
 
 (function (context) {
 
-    $("#dvCategories").kendoGrid({
-        dataSource: {
-            transport: {
-                read: "/Admin/GetCategories"
-            },
+    context.DataGrid = null;
+    context.CategoryWindow = null;
 
-            pageSize: 25
-        },
-        height: 550,
-        sortable: true,
-        selectable: true,
-        pageable: {
-            refresh: true,
-            pageSizes: true,
-            buttonCount: 5
-        },
-        columns: [{
-            field: "name",
-            title: "Название"
-        }, {
-            field: "title",
-            title: "Заголовок"
-        }, {
-            field: "description",
-            title: "Описание",
-            width: 350
-        }]
-    }).data("kendoGrid");
+    context.init = function () {
+
+        $(".as-nav-link[name=categories]").css("color", "#F46A1C");        
+
+        context.DataGrid = $("#dvCategories").kendoGrid({
+            dataSource: {
+                transport: {
+                    read: "/Admin/GetCategories"
+                },
+
+                pageSize: 25
+            },
+            height: 550,
+            sortable: true,
+            selectable: true,
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                buttonCount: 5
+            },
+            columns: [{
+                field: "name",
+                title: "Название"
+            }, {
+                field: "title",
+                title: "Заголовок"
+            }, {
+                field: "description",
+                title: "Описание"
+            }]
+        }).data("kendoGrid");
+
+        context.CategoryWindow = $("#add_window").kendoWindow().data("kendoWindow");
+        $("#add_category_btn").kendoButton({
+            click: function (e) {
+                context.CategoryWindow.center().open();
+            }
+        });
+    };    
 
 })(CategoryContext);
